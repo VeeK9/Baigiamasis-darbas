d335d65ad9713262cc5c0b3569021cdb50a4ef16
VeeK9 <vytautas.kakaras@gmail.com>
2024-03-25 14:28:29 +0200

    added json, and usersContext for logged in user to work

--- src/contexts/UsersContext.jsx
+++ src/contexts/UsersContext.jsx
@@ -1,12 +1,43 @@
-import { createContext } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 
 const UsersContext = createContext();
 
+const UsersActionTypes = {
+  GET_ALL: "fetches all data on inital load"
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case UsersActionTypes.GET_ALL:
+      return action.data;
+    default:
+      console.error(`No such action: ${action.type}`);
+      return state;
+  }
+}
+
 const UsersProvider = ({children}) => {
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const [users, setUsers] = useReducer(reducer, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/users`)
+      .then(res => res.json())
+      .then(data => setUsers({
+        type: UsersActionTypes.GET_ALL,
+        data: data
+      }))
+  }, [])
+
   return (
     <UsersContext.Provider
       value={{
-
+        users,
+        setUsers,
+        currentUser,
+        setCurrentUser
       }}
     >
       {children}
