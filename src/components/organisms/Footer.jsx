af62e9b15272550f81335b16b81f97bd355dccb0
VeeK9 <vytautas.kakaras@gmail.com>
2024-03-25 23:02:25 +0200

    added socials and some text to the footer

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,14 +1,57 @@
 import styled from "styled-components";
+import ListItem from "../UI/ListItem";
 
 const StyledFooter = styled.footer`
   height: 200px;
-  background-color: green;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  > ul {
+    list-style-type: none;
+    display: flex;
+    gap: 10px;
+    > li {
+      font-size: 1.5rem;
+      a {
+        color: black;
+      }
+    }
+  }
 `
 
 const Footer = () => {
+
+  const socials = [{
+    url: "https://www.strava.com",
+    icon: ['bi', ' bi-strava']
+  },{
+    url: "https://www.facebook.com",
+    icon: ['bi', ' bi-facebook']
+  },{
+    url: "https://www.instagram.com",
+    icon: ['bi', ' bi-instagram']
+  },{
+    url: "https://www.twitter.com",
+    icon: ['bi', ' bi-twitter-x']
+  },{
+    url: "https://www.youtube.com",
+    icon: ['bi', ' bi-youtube']
+  }];
+
   return (
     <StyledFooter>
-
+      <ul>
+        {
+          socials.map((social, idx) => 
+            <ListItem 
+              key={idx}
+              social={social}
+            />
+          )
+        }
+      </ul>
+      <p>Copyright © 2024 Stride Buddies. All rights reserved. Violators will be prosecuted.</p>
     </StyledFooter>
   );
 }
